39756b228700fc2b43c5b81235eac4efad07b1d4
JohnRyland <JohnRyland@users.noreply.github.com>
2022-04-16 07:56:40 +0000

    Publish Maths3D documentation

--- Documentation/files_dup.js
+++ Documentation/files_dup.js
@@ -1,5 +1,6 @@
 var files_dup =
 [
+    [ "docs", "dir_49e56c817e5e54854c35e136979f97ca.html", null ],
     [ "maths3d.h", "maths3d_8h.html", "maths3d_8h" ],
     [ "maths3d_ext.h", "maths3d__ext_8h.html", "maths3d__ext_8h" ],
     [ "tests.cpp", "tests_8cpp.html", "tests_8cpp" ]
