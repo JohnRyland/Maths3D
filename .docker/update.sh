881bac6815d102a30b4900fd3ee423ec09a6da8e
John <jryland@gmail.com>
2022-04-25 16:07:18 +1000

    create some different versions of the docker image

--- .docker/update.sh
+++ .docker/update.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-docker build -t dev-env .
+# Setup base docker image
+docker build -t dev-env -f Dockerfile.devenv .
 docker tag dev-env:latest jryland/dev-env:latest
 docker push jryland/dev-env:latest
+
+# Setup derived docker image for use in github workflows
+docker build -t dev-env-github -f Dockerfile.github .
+docker tag dev-env-github:latest jryland/dev-env-github:latest
+docker push jryland/dev-env-github:latest
+
+# Setup derived docker image for use in VSCode
+docker build -t dev-env-vscode -f Dockerfile.vscode .
+docker tag dev-env-vscode:latest jryland/dev-env-vscode:latest
+docker push jryland/dev-env-vscode:latest
+
