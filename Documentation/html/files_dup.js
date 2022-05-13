813404b68298c877a1d6316e3adf26c78a465140
JohnRyland <JohnRyland@users.noreply.github.com>
2022-05-13 15:12:49 +0000

    Publish documentation

--- Documentation/html/files_dup.js
+++ Documentation/html/files_dup.js
@@ -3,7 +3,7 @@ var files_dup =
     [ ".modules", "dir_4f65027d025cd4d9788b8ee69454eb75.html", "dir_4f65027d025cd4d9788b8ee69454eb75" ],
     [ "docs", "dir_49e56c817e5e54854c35e136979f97ca.html", null ],
     [ "examples", "dir_d28a4824dc47e487b107a5db32ef43c4.html", "dir_d28a4824dc47e487b107a5db32ef43c4" ],
-    [ "includes", "dir_09e761304027c904456130627fd4dcf5.html", "dir_09e761304027c904456130627fd4dcf5" ],
+    [ "include", "dir_d44c64559bbebec7f509842c48db8b23.html", "dir_d44c64559bbebec7f509842c48db8b23" ],
     [ "src", "dir_68267d1309a1af8e8297ef4c3efbcdba.html", "dir_68267d1309a1af8e8297ef4c3efbcdba" ],
     [ "tests", "dir_59425e443f801f1f2fd8bbe4959a3ccf.html", "dir_59425e443f801f1f2fd8bbe4959a3ccf" ]
 ];
